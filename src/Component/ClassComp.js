417207a87680ba7cff3aabb84f19ff127c3b41c8
Yogesh <yogeshzade999@gmail.com>
2022-07-30 01:58:40 +0530

    Add Component

---
+++ src/Component/ClassComp.js
@@ -0,0 +1,9 @@
+import React, {Component} from 'react';
+
+class ClassComp extends Component{
+    render(){
+        return <h2>This is Class component</h2>
+    }
+}
+
+export default ClassComp;
---
+++ src/Component/FunComp.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const FunComp = () =>{
+    return (
+        <div className='App'>
+            <h2>Function Component</h2>
+        </div>
+    );
+}
+
+export default FunComp;
